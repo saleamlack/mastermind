f11f2191e7cffd76209d39e412ea6228c68d344c
Saleamlack <saleamlack7@gmail.com>
2023-03-22 20:25:14 +0300

    Add `draw_board` which call other methods to draw
    To make the code more readable, I add `print_score`, `print_holes`,
    `print_codes`.
    - `print_score`: draw the section which shows the score of players
    - `print_holes`: draw the section which shows code and key holes
    - `print_codes`: draw the section which shows all codes pegs

--- board.rb
+++ board.rb
@@ -2,7 +2,7 @@
 
 # A class to draw, read, write the board
 class Board
-  attr_accessor :secret_code, :code_holes, :key_holes, :turn
+  attr_accessor :secret_code, :code_holes, :key_holes, :guess_turn, :shield
 
   def initialize
     self.secret_code = Array.new(4)
@@ -11,4 +11,44 @@ class Board
     self.guess_turn = 1
     self.shield = 'SHIELD'
   end
+
+  def draw_board
+    print_score
+    print_holes
+    print_codes
+  end
+
+  def print_score
+    puts "\e[44;1;2m         Score          \e[0m"
+    puts "\e[40m Human:           00    \e[0m"
+    puts "\e[40m Computer:        00    \e[0m"
+    puts "\e[33;2m========================\e[0m"
+  end
+
+  def print_holes
+    puts "\e[43m  \e[0m\e[44;1m #{@shield} \e[0m\e[43m    Feed    \e[0m"
+    code_holes.reverse.each_with_index do |holes, i|
+      comb = holes + key_holes.reverse[i]
+      print "\e[44m  \e[0m\e[45; \e[0m"
+      comb.each_with_index do |hole, j|
+        print " #{hole || '-'}"
+        print " \e[45;8m|\e[0m\e[44;8m|\e[0m" if j == 3
+        puts " \e[44;8m|\e[0m " if j == 7
+      end
+    end
+  end
+
+  def print_codes
+    puts "\e[33;1m========================\e[0m"
+    6.times do |i|
+      print "\e[40m   \e[0m" if [0, 3].include?(i)
+      print "\e[4#{i + 2}m #{i + 1} \e[0m" if i == 5
+      print "\e[4#{i + 1}m #{i + 1} \e[0m" unless i == 5
+      puts "\e[40m    Turn    \e[0m" if i == 2
+      puts "\e[40m     #{guess_turn}      \e[0m" if i == 5
+    end
+    puts "\e[33;2m========================\e[0m"
+  end
 end
+
+Board.new.draw_board
