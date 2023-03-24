ae6882570858a38909da2839e88937ac9e4e2f73
Saleamlack <saleamlack7@gmail.com>
2023-03-24 20:41:22 +0300

    Define `code_maker` to set code maker

--- game.rb
+++ game.rb
@@ -5,8 +5,19 @@ class Game
   attr_accessor :player1, :player2, :game_round
 
   def initialize(player1_obj, player2_obj)
-    self.player_1 = player1_obj
-    self.player_2 = player2_obj
+    self.player1 = player1_obj
+    self.player2 = player2_obj
     self.game_round = 1
+  end
+
+  def code_maker
+    puts 'Who make the code?'
+    puts "\t1. #{player1.name}"
+    puts "\t2. #{player2.name}"
+    if gets.chomp.to_i == 1
+      player1.code_maker = true
+    else
+      player2.code_maker = true
+    end
   end
 end
