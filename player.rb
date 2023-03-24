8f13e0b8b8f5714eddea22127d84c52dc8345ef9
Saleamlack <saleamlack7@gmail.com>
2023-03-24 18:14:55 +0300

    Add `HumanPlayer` class with `make_code` method
    `HumanPlayer` defines behavior of human player and it is inherited from
    player class.
    `make_code` help the player to make the secret code. I add
    `prompt_message` method to display the message that guides a player how
    to enter the secrete code or guess code. `prompt_message` is only called
    instance methods.

--- player.rb
+++ player.rb
@@ -3,9 +3,28 @@
 # A class to track state of players
 class Player
   attr_accessor :name, :score, :code_maker
+
   def initialize(name)
     self.name = name
     self.score = 0
     self.code_maker = false
+  end
+end
+
+# A class to define behaviors of human player
+class HumanPlayer < Player
+  def make_code
+    prompt_message
+    secret_code = gets.chomp.split(' ')
+  end
+
+  private
+
+  def prompt_message
+    puts 'Please enter your guess using numbers of each color.' unless self.code_maker
+    puts 'Please enter your secret code using numbers of each color.' if self.code_maker
+    puts 'Note: the numbers must be'
+    puts " - \e[31mIn correct oreder \e[37m(left-right)\e[0m\e[0m"
+    puts " - \e[31mSeparated by spaces.\e[37m(ex. 1 2 3 4)\e[0m\e[0m"
   end
 end
