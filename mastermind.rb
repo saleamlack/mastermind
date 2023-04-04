f7c89430e4d1dc15c9023e498dea4c1dc9560b24
Saleamlack <saleamlack7@gmail.com>
2023-04-04 19:33:23 +0300

    Add a module which contains instructions and messages
    `InstructionsAndText` provides methods for displaying instructions and
    messages to the user.
    `welcome` display message that provides information how to play the game
    `win_message` display winnig messages
    `lose_message` display winning messages
    `play_or_exit` ask a user if he want to play again or exit the game

--- mastermind.rb
+++ mastermind.rb
@@ -12,7 +12,7 @@ module Mastermind
     end
   end
 
-    # provides a method for counting the number of red and white pegs
+  # provides a method for counting the number of red and white pegs
   module CountPegs
     def count_key_pegs(guess_code, secret_code)
       white_pegs = 0
@@ -34,6 +34,42 @@ module Mastermind
       secret_code.each_with_object(Hash.new(0)) do |peg, counts|
         counts[peg] += 1
       end
+    end
+  end
+
+  # provides methods for displaying instructions and messages to the user
+  module InstructionAndText
+    def welcome
+      puts "\n\e[44mWelcome to Mastermind!\e[0m"
+      puts 'Mastermind game is a game for two players. One player create a secret code,'
+      puts 'the other player tries to guess the code for certain number of turns.'
+      puts "\n\e[34mHow to play\e[0m"
+      puts "- The codemaker create a code which is a combination of four colors.\n- The codebreaker guess the code"
+      puts '- the codebreaker get feedback for each guess'
+      puts "\t* \e[41mRED\e[0m for correct color and positon"
+      puts "\t* \e[47mWHITE\e[0m for correct color but not at the right position"
+      puts "\t* nothing for incorrect colors"
+    end
+
+    def win_message(human_player)
+      if human_player.code_maker
+        puts "\n\e[1mGreat job!\nYou have outsmarted the computer and won the game of Mastermind\e[0m"
+      else
+        puts "\n\e[1mCongratulations!\nYou are a code-breaking genius! Well done!\e[0m"
+      end
+    end
+
+    def lose_message(human_player)
+      if human_player.code_maker
+        puts "\n\e[1mOh no! The computer has cracked your code and won the game of Mastermind.\e[0m"
+      else
+        puts "\n\e[1mGame Over!\nSorry, you were not able to break the code this time. Better luck next time!\e[0m"
+      end
+    end
+
+    def play_or_exit
+      print "\e[34mDo you want to play again? (y/n)\e[0m"
+      gets.chomp
     end
   end
 end
