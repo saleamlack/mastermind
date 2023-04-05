bb02988d40981c0fa47d6aabdda507b9dcad13c5
Saleamlack <saleamlack7@gmail.com>
2023-04-05 11:29:56 +0300

    Add main game file and `PlayMastermind` class to start a new game

---
+++ main.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require_relative 'board'
+require_relative 'game'
+require_relative 'mastermind'
+require_relative 'player'
+
+# Create instance of new game and includes method to start the game
+class PlayMastermind
+  include Mastermind
+  include InstructionAndText
+
+  def play
+    game = Game.new(HumanPlayer.new, ComputerPlayer.new)
+    board = Borad.new
+    game.start_game(board)
+  rescue Interrupt
+    puts "\nExiting the game..."
+    exit(1)
+  end
+end
+
+new_game = PlayMastermind.new
+new_game.welcome
+new_game.play
