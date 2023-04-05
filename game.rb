341d6563efc3eede52847c8c53fcade5a6f43032
Saleamlack <saleamlack7@gmail.com>
2023-04-05 11:53:52 +0300

    Refactor Game class to include modules and improve functionality
    This commit refactors the `Game` class to include several modules:
    `Mastermind`, `MastermindConfig`, `CountPegs`, `InstructionAndText`, and
    `Validator`. These modules provide various methods for running the game,
    setting up the codemaker and code breaker, checking if the game is over,
    and validating user input.
    
    The code has been improved to eliminate redundant code and improve
    readability. The `code_maker` and `code_breaker` methods have been
    improved to provide better functionality, and the `setup_code`,
    `initialize_turn`, `end_game`, `give_feedback`, and `display_message`
    methods have been rewritten to imporve the overall gameplay experience.

--- game.rb
+++ game.rb
@@ -1,36 +1,125 @@
 # frozen_string_literal: true
 
-# Game: define required methods for the game
+require_relative 'mastermind'
+require_relative 'board'
+
+# provides methods for running the game, setting up codemaker and
+# codebreaker, checking if the game is over
 class Game
-  attr_accessor :player1, :player2, :game_round
+  include Mastermind
+  include MastermindConfig
+  include CountPegs
+  include InstructionAndText
+  include Validator
 
-  def initialize(player1_obj, player2_obj)
-    self.player1 = player1_obj
-    self.player2 = player2_obj
-    self.board = Board.new
-    self.game_round = 1
-    self.codemaker = nil
-    self.codebreaker = nil
+  attr_accessor :turn, :turn_guess, :codemaker, :codebreaker, :human_player
+  attr_reader :secret_code
+
+  def initialize(human_player, computer_player)
+    self.human_player = human_player
+    self.turn = 1
+    self.turn_guess = nil
+    self.codemaker = code_maker(human_player, computer_player)
+    self.codebreaker = code_breaker(human_player, computer_player)
+    @secret_code = nil
   end
 
-  def code_maker
-    puts "Who make the code?
-    1. #{player1.name}
-    2. #{player2.name}"
-    self.codemaker, self.codebreaker =
-      if gets.chomp.to_i == 1
-        [player1, player2]
-      else
-        [player2, player1]
+  def start_game(board)
+    setup_code(board)
+    initialize_turn(board)
+    end_game(board)
+  end
+
+  private
+
+  def setup_code(board)
+    board.draw_board(turn)
+    @secret_code = codemaker.make_code
+  end
+
+  def initialize_turn(board)
+    loop do
+      break if turn > MAX_TURN || broken?(turn_guess)
+
+      self.turn_guess =
+        if human_player.code_maker
+          codebreaker.guess_code(secret_code)
+        else
+          codebreaker.guess_code
+        end
+      feedback = give_feedback(turn_guess, secret_code)
+      board.get_played_codes(turn_guess, feedback)
+      board.draw_board(turn)
+      self.turn += 1
+    end
+  end
+
+  def end_game(board)
+    board.draw_board(turn - 1, secret_code)
+    display_message
+    play_or_exit == 'y' ? play_again : quit_game
+  end
+
+  def code_maker(human_player, computer_player)
+    selected_option = play_as_codemaker_codebreaker
+    if selected_option.to_i == 1
+      human_player.code_maker = true
+      human_player
+    else
+      computer_player
+    end
+  end
+
+  def code_breaker(human_player, computer_player)
+    human_player.code_maker ? computer_player : human_player
+  end
+
+  def play_as_codemaker_codebreaker
+    puts "\nWould you like to play as\n\t1. Codemaker or\n\t2. Codebreaker"
+    print 'Selection: '
+    selected_option = gets.chomp
+    if valid_player?(selected_option)
+      selected_option
+    else
+      puts "\e[31mIncorrect input! Please enter 1 or 2.\e[0m"
+      play_as_codemaker_codebreaker
+    end
+  end
+
+  def broken?(turn_guess)
+    secret_code == turn_guess
+  end
+
+  def give_feedback(guess_code, secret_code)
+    white_pegs, red_pegs = count_key_pegs(guess_code, secret_code)
+    guess_code.map do
+      if red_pegs.positive?
+        red_pegs -= 1
+        '2'
+      elsif white_pegs.positive?
+        white_pegs -= 1
+        '1'
       end
-    codemaker.code_maker = true
+    end
   end
 
-  def switch_codemaker
-    codemaker.code_maker = false
-    temp = codemaker
-    self.codemaker = codebreaker
-    self.codebreaker = temp
-    codemaker.code_maker = true
+  def display_message
+    if broken?(turn_guess) && human_player.code_maker
+      lose_message(human_player)
+    elsif broken?(turn_guess)
+      win_message(human_player)
+    else
+      lose_message(human_player)
+    end
+    puts 'Thank you!'
+  end
+
+  def play_again
+    system('clear') || system('cls')
+    PlayMastermind.new.play
+  end
+
+  def quit_game
+    exit(0)
   end
 end
